a0b99b8968cbef2f578466f63e507d135b5f414e
Carlos Cuellar <carturoch@gmail.com>
2017-03-03 16:29:05 -0500

    Implement Straight rank function.

--- lib/ex_poker_eval/rank.ex
+++ lib/ex_poker_eval/rank.ex
@@ -6,7 +6,28 @@ defmodule ExPokerEval.Rank do
   @doc """
   Gets a straight if found
   """
-  def get_straight(_hand) do
-    {}
+  def get_straight(hand) do
+    cards = hand
+    |> Enum.map(&(&1[:value]))
+    |> Enum.sort
+    |> ace_as_one
+
+    with lower <- cards |> List.first,
+      upper <- cards |> List.last,
+      sequence <- lower..upper |> Enum.to_list,
+      true <- cards == sequence
+    do
+      {:straight, upper}
+    else
+      _ -> {}
+    end
   end
+
+  @doc """
+  Helper to use Ace as value 1 when needed
+  to complete a lower straight.
+  """
+  def ace_as_one([2,3,4,5,14]), do: [1,2,3,4,5]
+  def ace_as_one(values), do: values
+
 end
--- test/ex_poker_eval/rank_test.exs
+++ test/ex_poker_eval/rank_test.exs
@@ -12,8 +12,8 @@ defmodule ExPokerEval.RankTest do
     [suit: "D", value: 13]
   ]
 
-  @straight_lowest_cards [
-    [suit: "H", value: "A"],
+  @straight_lower_cards [
+    [suit: "H", value: 14],
     [suit: "D", value: 2],
     [suit: "S", value: 3],
     [suit: "C", value: 4],
@@ -28,13 +28,29 @@ defmodule ExPokerEval.RankTest do
     [suit: "H", value: 6]
   ]
 
+  @straight_upper_cards [
+    [suit: "D", value: 10],
+    [suit: "S", value: 11],
+    [suit: "C", value: 12],
+    [suit: "D", value: 13],
+    [suit: "H", value: 14]
+  ]
+
   describe "get_straight" do
     test "return empty tuple if not found" do
       assert Rank.get_straight(@highcard_cards) == {}
     end
 
-    test "get the lower straight" do
-      assert Rank.get_straight(@straight_middle_cards) == {:straight, []}
+    test "get a straight whithout ace" do
+      assert Rank.get_straight(@straight_middle_cards) == {:straight, 6}
+    end
+
+    test "get upper straight" do
+      assert Rank.get_straight(@straight_upper_cards) == {:straight, 14}
+    end
+
+    test "get lower straight" do
+      assert Rank.get_straight(@straight_lower_cards) == {:straight, 5}
     end
   end
 end
